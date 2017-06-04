942c692c1cc688250a52e4897c68b1468c39dbd6
bonoj <johnlbono@gmail.com>
2017-06-04 01:21:06 -0500

    Testing Box2DLights.

--- core/src/com/hk47/arcana/ArcanaGame.java
+++ core/src/com/hk47/arcana/ArcanaGame.java
@@ -2,6 +2,7 @@ package com.hk47.arcana;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,6 +12,12 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+import box2dLight.Light;
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
 
 public class ArcanaGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -26,11 +33,15 @@ public class ArcanaGame extends ApplicationAdapter {
 	int levelPixelWidth;
 	int levelPixelHeight;
 
+	World world;
+	RayHandler rayHandler;
+	Light light;
+
 	@Override
 	public void create () {
 
 		// Sound test
-		SoundManager.playMusic("Winds Of Stories.ogg", 0.2f);
+		SoundManager.playMusic("Winds Of Stories.ogg", 0.1f);
 
 		batch = new SpriteBatch();
 
@@ -47,6 +58,8 @@ public class ArcanaGame extends ApplicationAdapter {
 		TextManager.setSpriteBatch(batch);
 		object = new GameObject("purporb.png", batch, width / 2, height / 2);
 
+		world = new World(new Vector2(0f, -9.8f), false);
+
 		player = new Player(new Texture("purporb.png"), 300, 400);
 
 		MapProperties properties = tiledMap.getProperties();
@@ -58,6 +71,12 @@ public class ArcanaGame extends ApplicationAdapter {
 		int tilePixelHeight = properties.get("tileheight", Integer.class);
 		levelPixelWidth = levelWidth * tilePixelWidth;
 		levelPixelHeight = levelHeight * tilePixelHeight;
+
+		rayHandler = new RayHandler(world);
+		rayHandler.setAmbientLight(0.1f, 0.1f, 0.1f, 0.25f);
+		light = new PointLight(rayHandler, 1000, Color.BLUE, 200, 100, 200);
+		light = new PointLight(rayHandler, 1000, Color.YELLOW, 200, 300, 200);
+		light = new PointLight(rayHandler, 1000, Color.PURPLE, 200, 500, 200);
 	}
 
 	@Override
@@ -89,11 +108,15 @@ public class ArcanaGame extends ApplicationAdapter {
 		TextManager.draw("FPS: " + Gdx.graphics.getFramesPerSecond() + " Time: " + Time.time, camera);
 
 		batch.end();
+
+		rayHandler.setCombinedMatrix(camera.combined.cpy());
+		rayHandler.updateAndRender();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
 		SoundManager.dispose();
+		light.dispose();
 	}
 }
