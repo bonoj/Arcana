19d6ffd9242f058db0edb3c1ee1d3d3d2aada639
bonoj <johnlbono@gmail.com>
2017-06-06 13:00:38 -0500

    Added temporary CameraSystem fix, must explore alternatives.

--- core/src/com/hk47/arcana/systems/CameraSystem.java
+++ core/src/com/hk47/arcana/systems/CameraSystem.java
@@ -3,6 +3,7 @@ package com.hk47.arcana.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.hk47.arcana.Level;
 import com.hk47.arcana.Mappers;
@@ -26,58 +27,24 @@ public class CameraSystem extends IteratingSystem {
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
-        // CameraComponent cameraComponent = Mappers.camera.get(entity);
         TransformComponent transformComponent = Mappers.transform.get(entity);
-
-
-        // TODO Give CameraSystem some idea about world coordinates?
-       //  camera.position.x = Math.min(Math.max(transformComponent.position.x, width / 2), levelPixelWidth - (width / 2));
-//		camera.position.y = Math.min(Math.max(object.y, height / 2), levelPixelHeight - (height / 2));
-
-//        float width = Gdx.graphics.getWidth();
-//		float height = Gdx.graphics.getHeight();
-
 
         camera.position.x = transformComponent.position.x;
         // camera.position.y = transformComponent.position.y;
-//        if (camera.position.x < level.levelPixelWidth / 2) {
-//
-//        }
 
-        if (camera.position.x - level.levelPixelWidth / 2 < 0) {
-            camera.position.x = level.levelPixelWidth / 2;
+        // TODO Allow Level.class to handle adding and detaching camera component from player instead.
+        if (camera.position.x - Gdx.graphics.getWidth() / 2 < 0) {
+            camera.position.x = Gdx.graphics.getWidth() / 2;
         }
 
+//        if (camera.position.x + width > level.levelPixelWidth) {
+//            camera.position.x = level.levelPixelWidth - width / 2;
+//        }
+
+//        if (camera.position.y - Gdx.graphics.getHeight() / 2 < 0) {
+//            camera.position.y = Gdx.graphics.getHeight() / 2;
+//        }
+
         camera.update();
-
-//        Gdx.app.log("Camera position", "X: " + camera.position.x + " Y: " + camera.position.y);
-//        Gdx.app.log("Level width in pixels", "" + level.levelPixelWidth);
-
-
-//        camera.position.x = Math.min(Math.max(transformComponent.position.x, width / 2), level.levelPixelWidth - (width / 2));
-
-//        if (cameraComponent.target == null) {
-//            return;
-//        }
-//
-//        TransformComponent transformComponent = Mappers.transform.get(cameraComponent.target);
-//
-//        if (transformComponent == null) {
-//            return;
-//        }
-
-//        cameraComponent.camera.translate(
-//                transformComponent.position.x, transformComponent.position.y);
-
-//        cameraComponent.camera.position.x =
-//                Math.max(cameraComponent.camera.position.x, transformComponent.position.x);
-//
-//        cameraComponent.camera.position.y =
-//                Math.max(cameraComponent.camera.position.y, transformComponent.position.y);
-
-//        Vector3 cameraVector = cameraComponent.camera.position;
-//
-//        cameraComponent.camera.position =
-//                Math.max(cameraComponent.camera.position, transformComponent.position);
     }
 }
